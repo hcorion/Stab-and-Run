ae7df0acbb3457d1fd6d7fd7a05545757c30f014
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-30 12:30:38 -0700

    Fixed movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,26 +3,25 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 	public float speed = 1f;
-	public float MaxJumpTime = 10000f;
+	public float MaxJumpTime = 1f;
 	public float JumpForce;
 	public float moveVertical;
 	public float moveHorizontal;
 	private float JumpTime = 0f;
-	private bool CanJump = false;
+	public bool canJump = true;
 	public Rigidbody2D player;
+	private float jumpTime;
+	public GameObject raycastPoint;
 	void Start () {
 		player = gameObject.GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (!CanJump)
-			JumpTime  -= Time.deltaTime;
-		Debug.Log (JumpTime);
-		if (JumpTime <= 0)
+		if (jumpTime > MaxJumpTime)
 		{
-			CanJump = true;
-			JumpTime  = MaxJumpTime;
+			canJump = false;
+			jumpTime = 0;
 		}
 	}
 	void FixedUpdate () {
@@ -30,11 +29,26 @@ public class Movement : MonoBehaviour {
 		moveHorizontal = Input.GetAxis ("Horizontal");
 		player.velocity = new Vector2 (moveVertical * speed, player.velocity.y);
 		*/
-		if (Input.GetKey (KeyCode.W) && CanJump == true)
+		if (canJump == false)
 		{
-			player.AddForce (new Vector2 (0, 1), ForceMode2D.Impulse);
-			CanJump = false;
-			JumpTime  = MaxJumpTime;
+			//RaycastHit2D
+			RaycastHit2D hit = Physics2D.Raycast(raycastPoint.transform.position, Vector2.down, .1f);
+			if (hit.collider != null)
+			{
+				Debug.Log("Something or other is " + hit.collider.name);
+				canJump = true;
+				jumpTime = 0;
+			}
+		{
+
+		}
+		}
+		if (Input.GetKey (KeyCode.W) && canJump == true)
+		{
+			player.AddForce (new Vector2 (0, .6f), ForceMode2D.Impulse);
+			jumpTime += Time.deltaTime;
+			//CanJump = false;
+			//JumpTime  = MaxJumpTime;
 		}
 		if(Input.GetKey(KeyCode.D)){
 			player.transform.position += Vector3.right * speed * Time.deltaTime;
---
+++ Assets/Scripts/winTriggers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class winTriggers : MonoBehaviour {
+
+	private bool isPlayerSet = false;
+	private GameObject myPlayer;
+	public GameObject playerStartPoint;
+	// Use this for initialization
+	void Start () {
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			if (!isPlayerSet)
+			{
+				myPlayer = col.gameObject;
+			}
+			else if (col.gameObject != myPlayer)
+			{
+				//The opposing player won!
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
