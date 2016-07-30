550796f865bfe40dcd86b2d1509d88813582210c
Queenie Zhang <Queenie Zhang>
2016-07-30 15:24:24 -0700

    sword collision with player

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void onTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player" && col.gameObject != transform.root) {
+			Destroy (col.gameObject);
+		}
+	}
+}
